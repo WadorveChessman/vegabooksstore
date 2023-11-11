b60df6c5cdd79c6c717477c8b6ae2c9e6e272db5
Wadorve <143746507+WadorveChessman@users.noreply.github.com>
2023-11-10 23:00:49 -0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Nous sommes Vega Books Store! ajouter /api pour voir le travail.';
+    return 'Nous sommes Vega Books Store! ajouter /api pour voir le travail effectuer.';
   }
 }
