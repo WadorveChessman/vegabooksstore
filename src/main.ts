d0c586b95ac0f6ec77259c0b19a7873cdcb227d3
Wadorve <143746507+WadorveChessman@users.noreply.github.com>
2023-11-10 22:58:43 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,37 +5,22 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const config = new DocumentBuilder()
     .setTitle('Vega Books Store')
     .setDescription(`
       <div style="text-align: center;">
         <b>Projet système réparti</b>
-        <p>Informatique niveau IV</p>
-        <p>UNAF</p>
+        <p>informatique niveau IV</p>
       </div>
     `)
     .setVersion('2.3')
     .addTag('Vega')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
-
-  // Personnalisation de l'apparence de SwaggerUI
-  const customOptions = {
-    customCss: `
-      .swagger-ui .topbar { text-align: center; }
-      .swagger-ui .info .title { text-align: center; }
-      .swagger-ui .info .description { text-align: center; }
-    `,
-    customSiteTitle: 'Vega Books Store 2.3', // Titre stylisé
-  };
-
-  SwaggerModule.setup('api', app, document, customOptions);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(8080);
 }
-
 bootstrap();
